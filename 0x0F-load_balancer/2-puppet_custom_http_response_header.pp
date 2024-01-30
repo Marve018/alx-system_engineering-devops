032923de1687842aa37dc7ba890e14299ec83488
Marve018 <okechukwuchukwuemeka018@gmail.com>
2024-01-30 11:40:40 +0100

    {feat}: Puppet Script configuring Nginx to add a custom HTTP header.

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,25 +1,8 @@
-# Install Nginx
-class { 'nginx':
-  manage_repo => true,
-}
-
-# Configure Nginx
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  mode    => '0644',
-  content => "# Managed by Puppet\nserver {\n\tlisten 80 default_server;\n\tlisten [::]:80 default_server;\n\n\troot /var/www/html;\n\tindex index.html index.htm;\n\n\tserver_name _;\n\n\tlocation / {\n\t\ttry_files \$uri \$uri/ =404;\n\t}\n\n\t# Custom HTTP Header\n\tadd_header X-Served-By \$hostname;\n}\n",
-  notify  => Service['nginx'],
-}
-
-# Enable Nginx default site
-link { '/etc/nginx/sites-enabled/000-default':
-  to      => '/etc/nginx/sites-available/default',
-  require => File['/etc/nginx/sites-available/default'],
-}
-
-# Restart Nginx
-service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['/etc/nginx/sites-available/default'],
+# Add a custom HTTP header using Puppet
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
+  provider => shell,
 }
