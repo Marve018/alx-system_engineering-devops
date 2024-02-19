0b07d75be28cb2b24433888c783cd810099602a0
Marve018 <okechukwuchukwuemeka018@gmail.com>
2024-02-19 23:25:31 +0100

    {feat}: script that interacts with API to get data

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Gathering data from an API """
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    # Rest Api url
+    url = "https://jsonplaceholder.typicode.com/"
+
+    # retrive the employee data info
+    response = requests.get(url + "users/{}".format(sys.argv[1]))
+    employee = response.json()
+    
+    # retrive todo list
+    response = requests.get(url + "todos",
+                        params={"userId": sys.argv[1]})
+    todos = response.json()
+
+    # using list comprehenssion to filter & store completed list
+    completed_tasks = [todo.get("title") for todo in todos
+                       if todo.get("completed") is True]
+    
+    # get employee name
+    employee_name = employee.get("name")
+
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employee_name, len(completed_tasks), len(todos)))
+    
+    # print the titles of completed tasks with indentation
+    [print("\t {}".format(c)) for c in completed_tasks]
