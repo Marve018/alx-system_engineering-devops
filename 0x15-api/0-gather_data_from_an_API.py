771d85f1117fe2571fa9436c9e4ed4acb759f2ae
Marve018 <okechukwuchukwuemeka018@gmail.com>
2024-02-19 23:54:55 +0100

    {chore}: PEP 8 style fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -12,21 +12,21 @@ if __name__ == "__main__":
     # retrive the employee data info
     response = requests.get(url + "users/{}".format(sys.argv[1]))
     employee = response.json()
-    
+
     # retrive todo list
-    response = requests.get(url + "todos",
-                        params={"userId": sys.argv[1]})
+    response = requests.get(url +
+                            "todos", params={"userId": sys.argv[1]})
     todos = response.json()
 
     # using list comprehenssion to filter & store completed list
     completed_tasks = [todo.get("title") for todo in todos
                        if todo.get("completed") is True]
-    
+
     # get employee name
     employee_name = employee.get("name")
 
     print("Employee {} is done with tasks({}/{}):"
           .format(employee_name, len(completed_tasks), len(todos)))
-    
+
     # print the titles of completed tasks with indentation
     [print("\t {}".format(c)) for c in completed_tasks]
